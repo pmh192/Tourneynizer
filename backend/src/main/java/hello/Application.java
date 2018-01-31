21bee1576b86c557b7bab840924aeebcea366b83
Porter <pmh192@gmail.com>
2018-01-31 14:45:26 -0800

    Set up means for connecting to database

--- backend/src/main/java/hello/Application.java
+++ backend/src/main/java/hello/Application.java
@@ -2,11 +2,15 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("DevSpringModule.xml");
+
         SpringApplication.run(Application.class, args);
     }
 }
