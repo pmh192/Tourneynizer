1874b3ea16c8dda3eec642f2dddb3b032dc62744
pmh192 <porter_haet@umail.ucsb.edu>
2018-01-31 22:20:47 -0800

    User model created

--- backend/src/main/java/com/dreamteam/tourneynizer/model/User.java
+++ backend/src/main/java/com/dreamteam/tourneynizer/model/User.java
@@ -1,14 +1,54 @@
 package com.dreamteam.tourneynizer.model;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.sql.Timestamp;
+import java.util.regex.Pattern;
+
 public class User {
+    private Long id;
+    private String email, name, hashedPassword;
+    private Timestamp timeCreated;
 
-    private String value;
+    private final static PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public String getValue() {
-        return value;
+    //https://stackoverflow.com/questions/8204680/java-regex-email
+    private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    public User(String email, String name, String hashedPassword) {
+        this(null, email, name, hashedPassword, null);
     }
 
-    public void setValue(String value) {
-        this.value = value;
+    public User(Long id, String email, String name, String hashedPassword, Timestamp timeCreated) {
+        this.id = id;
+        setEmail(email);
+        setName(name);
+        this.hashedPassword = hashedPassword;
+        this.timeCreated = timeCreated;
+    }
+
+    public void setPlaintextPassword(String password) {
+        this.hashedPassword = passwordEncoder.encode(password);
+    }
+
+    public boolean correctPassword(String password) {
+        return passwordEncoder.matches(password, this.hashedPassword);
+    }
+
+    private void setEmail(String email) {
+        if (email.length() >= 256) { throw new IllegalArgumentException("Email is too long"); }
+        if (!VALID_EMAIL_ADDRESS_REGEX.matcher(email).find()) {
+            throw new IllegalArgumentException("Email is invalid");
+        }
+
+        this.email = email;
+    }
+
+    private void setName(String name) {
+        if (name.length() >= 256) { throw new IllegalArgumentException("Name is too long"); }
+        if (name.isEmpty()) { throw new IllegalArgumentException("Name cannot be empty"); }
+        this.name = name;
     }
 }
--- backend/src/test/com/dreamteam/tourneynizer/model/UserTest.java
+++ backend/src/test/com/dreamteam/tourneynizer/model/UserTest.java
@@ -2,11 +2,47 @@ package com.dreamteam.tourneynizer.model;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class UserTest {
 
     @Test
     public void constructor() throws Exception {
-        User user = new User();
+        new User("person@place.com", "name", "hashedPassword");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorBadEmail() throws Exception {
+        new User("invalidEmail", "name", "hashedPassword");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorEmptyName() throws Exception {
+        new User("person@place.com", "", "hashedPassword");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorLongName() throws Exception {
+        StringBuilder name = new StringBuilder();
+        for (int i = 0; i < 256; i++) {
+            name.append("F");
+        }
+        new User("person@place.com", name.toString(), "hashedPassword");
+    }
+
+    @Test
+    public void passwordCorrectness() throws Exception {
+        User user = new User("person@place.com", "name", "hashedPassword");
+        user.setPlaintextPassword("password");
+
+        assertTrue(user.correctPassword("password"));
+        assertFalse(user.correctPassword("incorrect"));
+    }
+
+    @Test
+    public void passwordHashed() throws Exception {
+        assertTrue(true); // change when ready
     }
 
 }
