e677fe45e605d9e1d339b7cade143349aa2e571a
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2018-02-04 14:16:32 -0800

    Added register view and linked to login

--- ios/tournenizer/tournenizer/AppDelegate.swift
+++ ios/tournenizer/tournenizer/AppDelegate.swift
@@ -17,7 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds);
-        let viewController = LoginViewController();
+
+        let viewController = UINavigationController();
+        viewController.navigationBar.isHidden = true;
+        viewController.pushViewController(LoginViewController(), animated: false);
         window!.rootViewController = viewController;
         window!.makeKeyAndVisible();
         return true;
--- ios/tournenizer/tournenizer/Config/Constants.swift
+++ ios/tournenizer/tournenizer/Config/Constants.swift
@@ -8,13 +8,17 @@
 
 import UIKit;
 
-class Constant {
+class Constants {
     static let red = UIColor.init(r: 231, g: 76, b: 60, a: 255);
     static let lightBlue = UIColor.init(r: 52, g: 152, b: 219, a: 255);
     static let white = UIColor.init(r: 236, g: 240, b: 241, a: 255);
     static let navy = UIColor.init(r: 44, g: 62, b: 80, a: 255);
     static let blue = UIColor.init(r: 41, g: 128, b: 185, a: 255);
-    
+
     static let fontMedium = "Futura-Medium";
     static let font = "Futura";
+
+    static let headerFontSize: CGFloat = 45;
+    static let normalFontSize: CGFloat = 17;
+    static let smallFontSize: CGFloat = 12;
 };
