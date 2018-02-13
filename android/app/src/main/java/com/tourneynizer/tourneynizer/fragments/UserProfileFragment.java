4662f68a8a00dde09c94a7cbaa1c03fc24259540
Ryan Wiener <ryanl.wiener@yahoo.com>
2018-02-13 15:59:30 -0800

    Set up basic layout for fragments

--- android/app/src/main/java/com/tourneynizer/tourneynizer/fragments/UserProfileFragment.java
+++ android/app/src/main/java/com/tourneynizer/tourneynizer/fragments/UserProfileFragment.java
@@ -9,48 +9,47 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.tourneynizer.tourneynizer.R;
+import com.tourneynizer.tourneynizer.data.User;
 
 public class UserProfileFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
+    private static final String USER = "com.tourneynizer.tourneynizer.data.User";
+
+    private User user;
 
     public UserProfileFragment() {
         // Required empty public constructor
     }
 
-    public static UserProfileFragment newInstance() {
+    public static UserProfileFragment newInstance(User user) {
         UserProfileFragment fragment = new UserProfileFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(USER, user);
+        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            user = getArguments().getParcelable(USER);
+        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user_profile, container, false);
     }
 
-
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    public interface OnFragmentInteractionListener {
-        void onFragmentInteraction(Uri uri);
     }
 }
