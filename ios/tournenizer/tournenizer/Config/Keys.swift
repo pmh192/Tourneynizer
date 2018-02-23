39a72a8e9429a04ae0976bda786405c4e211165b
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2018-02-22 22:34:16 -0800

    Added keys file

---
+++ ios/tournenizer/tournenizer/Config/Keys.swift
@@ -0,0 +1,13 @@
+//
+//  Keys.swift
+//  tournenizer
+//
+//  Created by Ankush Rayabhari on 2/22/18.
+//  Copyright © 2018 Ankush Rayabhari. All rights reserved.
+//
+
+import Foundation
+
+class Keys {
+    static let googleMapsKey = "AIzaSyCW9dTC5-PC8gip02v2jIA9GTyubjBmTKI";
+};
