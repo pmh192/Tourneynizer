227b6b62a2950275a5f09ad319126cfe92df4f2b
Nick <ncduncan@umail.ucsb.eduncduncan@umail.ucsb.edu>
2018-03-04 22:58:03 -0800

    login persists through refresh + tournament fetch works

--- website/src/Components/PageComponents/TournamentCreatePage.jsx
+++ website/src/Components/PageComponents/TournamentCreatePage.jsx
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import { Jumbotron } from 'react-bootstrap';
 
 class TournamentCreatePage extends Component{
+
+	componentWillMount(){
+		let apiURL='http://localhost:8080/api/tournament/create';
+		 let data={
+		 	"name":'test',
+		 	"address":'test',
+		 	"startTime":0,
+		 	"teamSize":0,
+		 	"maxTeams":0,
+		 	"type":1,
+		 	"numCourts":0,
+		 }
+
+		fetch(apiURL,{
+			method: 'POST',
+			body: JSON.stringify(data),
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+ 			credentials: 'include',
+			
+		})
+	}
 	render(){
 		return(
 			<div>
