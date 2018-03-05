725c9260c9fac3f0048e09768015f48f2ccdfe5a
Evan Murray <evanmurray@umail.ucsb.edu>
2018-03-04 18:51:45 -0800

    updated rules page on website

--- website/src/Components/PageComponents/TournamentRulesPage.jsx
+++ website/src/Components/PageComponents/TournamentRulesPage.jsx
@@ -4,6 +4,10 @@ class TournamentRulesPage extends Component{
 	render(){
 		return(
 			<div>
+				<Jumbotron>
+					<h1>Tournament Rules Document</h1>
+					<a href="https://cbva.com/UploadDocuments/CBVADocs/CBVARulebook.pdf">Rules</a>
+				</Jumbotron>
 			</div>
 		);
 	}
