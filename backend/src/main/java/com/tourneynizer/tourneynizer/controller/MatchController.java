d8b9643fe9ee798181f9681d357ade82b4416dfd
pmh192 <porter_haet@umail.ucsb.edu>
2018-03-09 20:04:07 -0800

    Expose match status enum

--- backend/src/main/java/com/tourneynizer/tourneynizer/controller/MatchController.java
+++ backend/src/main/java/com/tourneynizer/tourneynizer/controller/MatchController.java
@@ -4,6 +4,7 @@ import com.tourneynizer.tourneynizer.error.BadRequestException;
 import com.tourneynizer.tourneynizer.error.InternalErrorException;
 import com.tourneynizer.tourneynizer.model.ErrorMessage;
 import com.tourneynizer.tourneynizer.model.Match;
+import com.tourneynizer.tourneynizer.model.MatchStatus;
 import com.tourneynizer.tourneynizer.service.MatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -14,7 +15,8 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Controller("MatchController")
 public class MatchController {
@@ -36,5 +38,25 @@ public class MatchController {
         } catch (InternalErrorException e) {
             return new ResponseEntity<Object>(new ErrorMessage(e), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    private static final Map<String, Object> toReturn = getToReturn();
+
+    private static Map<String, Object> getToReturn() {
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("actual", MatchStatus.values());
+
+        Map<MatchStatus, String> converter = MatchStatus.getStringMap();
+        map.put("user", Arrays.stream(MatchStatus.values())
+                .map(converter::get)
+                .collect(Collectors.toList())
+        );
+        return map;
+    }
+
+    @GetMapping("/api/enum/match/status")
+    public ResponseEntity<?> getEnum() {
+        return new ResponseEntity<Object>(toReturn, new HttpHeaders(), HttpStatus.OK);
     }
 }
